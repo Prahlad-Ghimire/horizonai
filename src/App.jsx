dbd63e7ccd2496e9199c28fd8638739c02a8067d
Prahlad-Ghimire <parhladghimire4545@gmail.com>
2025-03-29 22:50:31 +0545

    folderadding

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Sidebar from './components/sidebar/sidebar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fontsource/roboto';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Sidebar />
+    </>
   )
 }
 
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import './sidebar.css'
+import React, { useState } from 'react'
+import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
-const sidebar = () => {
-  return (
-    <div className='sidebar'>
-      
-    </div>
-  )
+const Sidebar = () => {
+    const [extended, setextended] = useState(false)
+    return (
+        <div>
+            <div className='sidebar d-inline-flex flex-column justify-content-between'>
+                <div className='top'>
+                    <img onClick={()=>setextended(prev => !prev)} src={assets.menu_icon} alt="" className='menu d-block ' />
+                    <div className='newchat d-inline-flex gap-2 align-items-center rounded-pill'>
+                        <img src={assets.plus_icon} alt="" />
+                        {extended ? <p className='text-capitalize fw-bold m-0'>new chat</p> : null}
+                    </div>
+                    {extended ?
+                        <div className='recent d-flex flex-column'>
+                            <p className='title mb-0'>Recent</p>
+                            <div className='recententry d-flex align-items-center gap-2 rounded-pill'>
+                                <img src={assets.message_icon} alt="" />
+                                <p className='text-capitalize fw-bold mb-0'>what is react</p>
+                            </div>
+                        </div> : null}
+                </div>
+                <div className='bottom d-flex flex-column'>
+                    <div className='bottomitems recententry d-flex align-items-center gap-2 rounded-pill'>
+                        <img src={assets.question_icon} alt="" />
+                        {extended?<p className='text-capitalize mb-0'>Help</p>:null}
+                    </div>
+                    <div className='bottomitems recententry d-flex align-items-center gap-2 rounded-pill'>
+                        <img src={assets.history_icon} alt="" />
+                        {extended? <p className='text-capitalize mb-0'>activity</p>:null}
+                    </div>
+                    <div className='bottomitems recententry d-flex align-items-center gap-2 rounded-pill'>
+                        <img src={assets.setting_icon} alt="" />
+                        {extended? <p className='text-capitalize mb-0'>Setting</p> :null}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default sidebar
+export default Sidebar
